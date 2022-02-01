5397d4b5902b65a2c5842dc45cef59a82d681913
Артем Громов <freemirror@ya.ru>
2022-02-01 19:18:59 +0300

    change 28 pract

--- practice28.py
+++ practice28.py
@@ -1,6 +1,5 @@
 import random
 num = 0
-for count in range(0, 7):
-    num = random.randint(10, 99)
+for count in range(0, 20):
+    num = random.randint(10, 999)
     print(num)
-print(random.randint(99, 999))
